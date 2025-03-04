f1cdb036fea47fd11b267740f227deed7b0e6d26
Neena Varughese <158041703+neenavarughese55@users.noreply.github.com>
2025-03-04 16:38:23 +0000

    updated wordle and added the union street background

--- wordle.js
+++ wordle.js
@@ -1,24 +1,37 @@
 const words = ["nandos"];
 
+let wordKeys = Object.keys(words);
 let word = words[Math.floor(Math.random() * words.length)];
+
 console.log(word);
 
 const submitBtn = document.getElementById("submit-btn");
 const guessInput = document.getElementById("guess");
 const guessList = document.getElementById("guess-list");
+const errorMsg = document.getElementById("error-msg");
+const attemptsLeft = document.getElementById("attempts-left");
 
 let attempts = 0;
+const maxAttempts = 6;
 
 submitBtn.addEventListener("click", function () {
     const guess = guessInput.value.toLowerCase();
     let result = "";
-    attempts++;
 
-    if (guess.length !== word.length) {
+    if (!/^[a-zA-Z]+$/.test(guess)) {
+        errorMsg.textContent = "Invalid input! Only letters are allowed.";
+        return;
+    }
+
+
+    errorMsg.textContent = ""; // Clear error message if input is valid
+
+    if (guess.length !== 6) {
         result = `Please enter a 6-letter word`;
     } else if (guess === word) {
         result = "You win!";
         submitBtn.disabled = true;
+        guessInput.disables = true;
     } else {
         const letters = word.split("");
         for (let i = 0; i < letters.length; i++) {
@@ -36,6 +49,15 @@ submitBtn.addEventListener("click", function () {
     const listItem = document.createElement("li");
     listItem.textContent = `${guess} - ${result}`;
     guessList.appendChild(listItem);
+
+    attempts++;
+    attemptsLeft.textContent = `Attempts Left: ${maxAttempts - attempts}`;
+
+    if (attempts >= maxAttempts && guess !== word) {
+        errorMsg.textContent = 'Game Over!';
+        submitBtn.disabled = true;
+        guessInput.disabled = true;
+    }
     
     // Clear input
     guessInput.value = "";
